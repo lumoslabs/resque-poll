074b9ce5c629dbdb85fb4ea42ef25e8d6e32382e
Anthony Zacharakis <anthony@lumoslabs.com>
2014-07-16 17:30:10 -0700

    Add ResquePoll::Job model to make job creation easier

---
+++ app/models/resque_poll/job.rb
@@ -0,0 +1,27 @@
+class ResquePoll::Job
+  attr_reader :klass, :params, :job_id
+
+  def initialize(klass, params)
+    @klass  = klass
+    @params = params
+  end
+
+  def self.create(*args)
+    new(*args).create
+  end
+
+  def create
+    @job_id = klass.create(params)
+    self
+  end
+
+  def as_json(options = {})
+    {poll: job_path}
+  end
+
+  private
+
+  def job_path
+    job_id ? ResquePoll::Engine.routes.url_helpers.job_path(id: job_id) : nil
+  end
+end
---
+++ spec/models/resque_poll/job_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe ResquePoll::Job do
+  class DoesNothingJob
+    def self.create(*args); end
+  end
+
+  let(:job) { ResquePoll::Job.new(DoesNothingJob, args) }
+  let(:args) { {foo: 'bar'} }
+
+  describe '#create' do
+    subject { job.create }
+
+    it 'creates the resque job' do
+      DoesNothingJob.should_receive(:create).with(args)
+      subject
+    end
+
+    it 'returns self' do
+      subject.should == job
+    end
+  end
+
+  describe '#as_json' do
+    subject { job.as_json }
+
+    context 'without a job id' do
+      it 'does not have a poll attribute' do
+        expect(subject[:poll]).to be_nil
+      end
+    end
+
+    context 'with a job id' do
+      before { job.stub(job_id: 'some-job-id') }
+
+      it 'has a poll attribute' do
+        expect(subject[:poll]).to be_present
+      end
+    end
+  end
+end
